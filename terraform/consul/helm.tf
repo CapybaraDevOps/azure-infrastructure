2e47b8e7e9ffa91abf9d0d4d3cc6020830a1cce0
AlanaSahaidak <sahaidak_alana@ukr.net>
2024-07-18 14:38:27 +0300

    Added helm_release

---
+++ terraform/consul/helm.tf
@@ -0,0 +1,16 @@
+resource "helm_release" "consul" {
+  name       = "consul"
+  repository = "https://helm.releases.hashicorp.com"
+  chart      = "consul"
+  namespace  = kubernetes_namespace.consul.metadata[0].name
+  #version    = "1.2.0"
+
+  values = [
+    file("helm/values-v1.yaml")
+  ]
+
+  set {
+    name  = "global.datacenter"
+    value = "dev-capybara-01"
+  }
+}
